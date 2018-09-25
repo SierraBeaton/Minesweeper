c6b10948db76fc2a0830e058272ffbf069857cc2
SierraBeaton <srb17a@acu.edu>
2018-09-24 19:13:52 -0500

    Putting stuff in there.

---
+++ app/src/main/java/com/example/sierrabeaton/minesweeper/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.sierrabeaton.minesweeper;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ((TextView)findViewById(R.id.textView)).setText("ASDASDASDASD");
+
+        Log.e("MainActivity","onCreate");
+        GameEngine.getInstance().createGrid(this);
+    }
+}
